2a312c4221c99fa56349cc0cc50c1093581ded35
arjaygg <arjay.gallentes@olea.net>
2023-08-13 12:35:42 +0800

    feat: change to have the macos support
    - Must have the following packages
    brew install caarlos0/tap/timer
    brew install terminal-notifier

--- system/functions/pomodoro.fish
+++ system/functions/pomodoro.fish
@@ -1,10 +1,10 @@
 function pomodoro -d "a cli pomodoro"
 	while true
 		say "Let's go!"
-		timer 25m --name Working
+		timer 50m --name Working
 		say "Break!"
-		notify-send "Time to rest..." -a Pomodoro -c Done 		
-		timer 5m --name Break
-		notify-send "Let's get back to it" -a Pomodoro -c Work
+		terminal-notifier -message "Time to rest..." -title Pomodoro -subtitle "Done!" -ignoreDnd -sound default
+		timer 10m --name Break
+		terminal-notifier -message "Let's get back to it" -title Pomodoro -subtitle "Let's go!" -ignoreDnd -sound default
 	end
 end
